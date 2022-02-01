8b6212a09b5ed8d967bd52e8763c0d5517725abc
maggie w <margaret.wooten@nasa.gov>
2022-01-31 19:54:29 -0500

    finished footprint code, problems with cross-meridian polygons still exist

--- create_atl08_v005_footprints.py
+++ create_atl08_v005_footprints.py
@@ -6,6 +6,11 @@ Created on Fri Jan 28 17:46:02 2022
 
 Using the extentIndex.csv, which has the file name and min/max extents,
 make a footprints .shp of ATL08 v005 files
+
+# SEE:
+https://gis.stackexchange.com/questions/294206/create-a-polygon-from-coordinates-in-geopandas-with-python
+https://gis.stackexchange.com/questions/285336/convert-polygon-bounding-box-to-geodataframe
+
 """
 
 import os, sys
@@ -14,13 +19,23 @@ import pandas as pd
 import geopandas as gpd
 
 import shapely
+from shapely.geometry import box
 
 
 
 indexCsv = '/att/gpfsfs/briskfs01/ppl/mwooten3/3DSI/ATL08_v005/ATL08_v005_h5__extentIndex-test.csv'
 outCsv = indexCsv.replace('extentIndex.csv', 'footprints.shp')
 
-gdf = gpd.read_file(indexCsv)
-gdf.crs = 'epsg:4326'
+# Read .csv into regular dataframe
+df = pd.read_csv(indexCsv)
+
+# Using the extent columns and box, build a polygon array
+polys = df.apply(lambda row: box(row.xmin, row.ymin, row.xmax, row.ymax), axis=1)
 
 import pdb; pdb.set_trace()
+
+
+
+
+# Now create a geodataframe from df
+gdf = gpd.GeoDataFrame(df, geometry = polys, crs = 'EPSG:4326')
